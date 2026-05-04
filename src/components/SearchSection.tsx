b7e32dbfa01a5e85177f351277e4a4c43045738a
Kostiantyn Kikinov <kikinov.k@gmail.com>
2026-05-04 17:57:54 +0300

    fix: add favicon

--- src/components/SearchSection.tsx
+++ src/components/SearchSection.tsx
@@ -10,7 +10,7 @@ class SearchSection extends Component<SearchSectionProps> {
     return (
       <section className="w-full px-8 py-6 border-b border-midnight-core flex flex-col gap-4">
         <h1 className="text-heading-lg font-noigrotesk text-stardust tracking-tight">
-          RSS React 2026 Q2
+          Pokémon Search
         </h1>
         <SearchBar onSearch={this.props.onSearch} />
       </section>
