ab92626e8a362c93f6622fdaf558bf8ec9aabbeb
Kostiantyn Kikinov <kikinov.k@gmail.com>
2026-05-05 16:06:59 +0300

    init: add hasky

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,27 +1,30 @@
 import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import react from 'eslint-plugin-react'
+import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import prettier from 'eslint-plugin-prettier/recommended'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import reactPlugin from 'eslint-plugin-react'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config(
-  { ignores: ['dist', 'build'] },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettier,
+export default defineConfig([
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      globals: globals.browser,
     },
     settings: {
       react: { version: 'detect' },
     },
-    rules: {
-      ...react.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-    },
-  }
-)
+  },
+])
