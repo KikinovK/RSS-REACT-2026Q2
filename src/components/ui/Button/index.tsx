c08a60d844abc481d583fd0e75f87857c8959893
Kostiantyn Kikinov <kikinov.k@gmail.com>
2026-06-06 18:03:45 +0300

    feat: add Button component

---
+++ src/components/ui/Button/index.tsx
@@ -0,0 +1,32 @@
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'icon';
+  children: ReactNode;
+}
+
+const Button = ({
+  children,
+  type = 'button',
+  variant = 'primary',
+  className,
+  ...props
+}: ButtonProps) => {
+  const variantClasses = {
+    primary:
+      'px-6 py-3 rounded-(--radius-buttons) bg-guidepost-green border-guidepost-green font-semibold text-deep-space text-body',
+    icon: 'h-10 w-10 p-2 text-guidepost-green rounded-full flex items-center justify-center',
+  };
+
+  return (
+    <button
+      {...props}
+      type={type}
+      className={`${className} ${variantClasses[variant]}   whitespace-nowrap transition-all hover:cursor-pointer hover:brightness-90 active:brightness-75 active:scale-95 disabled:opacity-40 disabled:cursor-not-allowed`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
