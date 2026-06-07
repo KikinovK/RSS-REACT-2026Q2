e6ba0b472eda707a07010f4b7c6460476b3266b5
Kostiantyn Kikinov <kikinov.k@gmail.com>
2026-06-07 23:16:18 +0300

    fix: button

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -15,14 +15,14 @@ const Button = ({
   const variantClasses = {
     primary:
       'px-6 py-3 rounded-(--radius-buttons) bg-guidepost-green border-guidepost-green font-semibold text-deep-space text-body',
-    icon: 'h-10 w-10 p-2 text-guidepost-green rounded-full flex items-center justify-center',
+    icon: 'h-10 w-10 p-3 bg-transparent text-guidepost-green rounded-full flex items-center justify-center hover:bg-guidepost-green/10',
   };
 
   return (
     <button
       {...props}
       type={type}
-      className={`${className} ${variantClasses[variant]}   whitespace-nowrap transition-all hover:cursor-pointer hover:brightness-90 active:brightness-75 active:scale-95 disabled:opacity-40 disabled:cursor-not-allowed`}
+      className={`${className || ''} ${variantClasses[variant]}   whitespace-nowrap transition-all hover:cursor-pointer hover:brightness-90 active:brightness-75 active:scale-95 disabled:opacity-40 disabled:cursor-not-allowed`}
     >
       {children}
     </button>
